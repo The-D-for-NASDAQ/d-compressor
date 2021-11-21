6aac607bbd042c976cda1e8a551eeb51fc48148a
Yevheniy <yevheniy2407@gmail.com>
2021-11-21 15:00:05 +0200

    add processing time timer

--- csv_to_matrices.py
+++ csv_to_matrices.py
@@ -170,9 +170,11 @@ def get_d(num_layers, num_price_levels, minutes_per_day, full_d_asks, full_d_bid
 
         highest_bid_price = max(highest_bid_prices)
 
-        for l_index in range(0, num_layers):
+        for l_index in range(0, num_layers - 1):
             d[l_index, 0:20, t_index] = np.flip(full_d_asks[l_index, lowest_ask_price:lowest_ask_price + 20, t_index])
             d[l_index, 20:40, t_index] = np.flip(
                 full_d_bids[l_index, highest_bid_price - 20:highest_bid_price, t_index])
+
+        d[num_layers - 1, :, t_index] = t_index
 
     return d
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import numpy as np
 import progress
 import csv_to_matrices
@@ -11,7 +12,7 @@ side_num_layers = 5  # Price, Ordered volume, Filled volume, Canceled volume, Pe
 side_num_price_levels = 500 * 2  # price level ($500), 50 cents per level (*2)
 side_minutes_per_day = 16 * 60  # 960, 16 hours of data per day, from 4:00 to 20:00
 
-d_num_layers = 5  # Price, Ordered volume, Filled volume, Canceled volume, Pending volume
+d_num_layers = 6  # Price, Ordered volume, Filled volume, Canceled volume, Pending volume, Time index
 d_num_price_levels = 10 * 2 * 2  # price level ($10) per 50 cents per level (*2) per side (*2)
 d_minutes_per_day = int(6.5 * 60)  # 6 hours 30 minutes of data per trading session, from 9:30 to 16:00
 
@@ -31,10 +32,13 @@ def main(date):
 
 if __name__ == '__main__':
     while True:
-        date_to_process = progress.get_process_date()
+        begin_time = datetime.datetime.now()
 
+        date_to_process = progress.get_process_date()
         print(date_to_process)
+
         main(date_to_process)
 
-        print('processed')
         progress.save_processed_date(date_to_process)
+
+        print('processing time: ' + str(datetime.datetime.now() - begin_time))
