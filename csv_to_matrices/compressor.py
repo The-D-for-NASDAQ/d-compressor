fix filling d with wrong values (depend on start of trading session); add compressing data by minutes

csv_to_matrices/compressor.py
@@ -12,7 +12,7 @@ def load_csv_file(filename):
     records['DateTime'] = pd.to_datetime(records['DateTime'], format='%Y%m%d %H:%M:%S.%f')
     records['Timestamp'] = records['DateTime'].values.astype(np.int64) // 10 ** 9
 
-    start_day_timestamp = int(records['DateTime'].iloc[0].timestamp())
+    start_day_timestamp = int(records['DateTime'].iloc[0].replace(hour=4, minute=0, second=0, microsecond=0).timestamp())
     records['time_index'] = np.int32((records['Timestamp'] - start_day_timestamp) / 60)  # index per minute
 
     records['price_level'] = np.int32(records['Price'] * 2)  # index per minute
@@ -58,9 +58,7 @@ def process_side_records(side, floor_price_round, num_layers, num_price_levels, 
 
     side_matrices = fill_matrices_price_level(side_matrices, num_price_levels, minutes_per_day, floor_price_round)
 
-    full_d_records = records \
-        .loc[records['price_level'] < num_price_levels] \
-        .loc[records['time_index'] < minutes_per_day]
+    full_d_records = records.loc[records['price_level'] < num_price_levels]
 
     side_matrices = process_simple_side_records(side, side_matrices, full_d_records)
     side_matrices = process_complex_side_records(side, side_matrices, full_d_records)
@@ -157,15 +155,17 @@ def process_pending_matrices_layer(matrices, minutes_per_day, num_price_levels):
     return matrices
 
 
-def get_d(num_layers, num_price_levels, minutes_per_day, full_d_asks, full_d_bids):
+def get_d(num_layers, num_price_levels, minutes_per_day, full_d_asks, full_d_bids, side_start_trading_session_index):
     d = create_zeros_array(num_layers, num_price_levels, minutes_per_day)
 
-    for t_index in range(0, minutes_per_day):
+    for d_t_index in range(0, minutes_per_day):
+        records_t_index = d_t_index + side_start_trading_session_index
+
         lowest_ask_prices = []
-        ordered_asks = np.where(full_d_asks[1, :, t_index] > 0)
-        filled_asks = np.where(full_d_asks[2, :, t_index] > 0)
-        canceled_asks = np.where(full_d_asks[3, :, t_index] > 0)
-        pending_asks = np.where(full_d_asks[4, :, t_index] > 0)
+        ordered_asks = np.where(full_d_asks[1, :, records_t_index] > 0)
+        filled_asks = np.where(full_d_asks[2, :, records_t_index] > 0)
+        canceled_asks = np.where(full_d_asks[3, :, records_t_index] > 0)
+        pending_asks = np.where(full_d_asks[4, :, records_t_index] > 0)
 
         if np.any(ordered_asks):
             lowest_ask_prices.append(ordered_asks[0][0])
@@ -180,10 +180,10 @@ def get_d(num_layers, num_price_levels, minutes_per_day, full_d_asks, full_d_bid
 
 
         highest_bid_prices = []
-        ordered_bids = np.where(full_d_bids[1, :, t_index] > 0)
-        filled_bids = np.where(full_d_bids[2, :, t_index] > 0)
-        canceled_bids = np.where(full_d_bids[3, :, t_index] > 0)
-        pending_bids = np.where(full_d_bids[4, :, t_index] > 0)
+        ordered_bids = np.where(full_d_bids[1, :, records_t_index] > 0)
+        filled_bids = np.where(full_d_bids[2, :, records_t_index] > 0)
+        canceled_bids = np.where(full_d_bids[3, :, records_t_index] > 0)
+        pending_bids = np.where(full_d_bids[4, :, records_t_index] > 0)
 
         if np.any(ordered_bids):
             highest_bid_prices.append(ordered_bids[0][0])
@@ -199,13 +199,13 @@ def get_d(num_layers, num_price_levels, minutes_per_day, full_d_asks, full_d_bid
         for l_index in range(0, num_layers - 1):
             # if lowest ask price missing, price slice will be from 0 to 20
             # if highest bid price missing, price slice will be from (max) - 20 to (max)
-            d[l_index, 0:20, t_index] = np.flip(full_d_asks[l_index, lowest_ask_price:lowest_ask_price + 20, t_index])
+            d[l_index, 0:20, d_t_index] = np.flip(full_d_asks[l_index, lowest_ask_price:lowest_ask_price + 20, records_t_index])
 
             if highest_bid_price - 20 < 0:
                 highest_bid_price = -1
 
-            d[l_index, 20:40, t_index] = np.flip(full_d_bids[l_index, highest_bid_price - 20:highest_bid_price, t_index])
+            d[l_index, 20:40, d_t_index] = np.flip(full_d_bids[l_index, highest_bid_price - 20:highest_bid_price, records_t_index])
 
-        d[num_layers - 1, :, t_index] = t_index
+        d[num_layers - 1, :, d_t_index] = d_t_index
 
     return d

main_daily_compressor.py
@@ -1,4 +1,4 @@
-import csv_to_matrices
+from csv_to_matrices import compressor
 import datetime
 import numpy as np
 import os
@@ -12,6 +12,7 @@ floor_price_round = 2 / (2 * 2)  # 0.5
 side_num_layers = 5  # Price, Ordered volume, Filled volume, Canceled volume, Pending volume
 side_num_price_levels = 500 * 2  # price level ($500), 50 cents per level (*2)
 side_minutes_per_day = 16 * 60  # 960, 16 hours of data per day, from 4:00 to 20:00
+side_start_trading_session_index = int(5.5 * 60)  # 330, from 4:00 to 9:30
 
 d_num_layers = 6  # Price, Ordered volume, Filled volume, Canceled volume, Pending volume, Time index
 d_num_price_levels = 10 * 2 * 2  # price level ($10) per 50 cents per level (*2) per side (*2)
@@ -20,14 +21,15 @@ d_minutes_per_day = int(6.5 * 60)  # 6 hours 30 minutes of data per trading sess
 
 def main(date):
     csv_data_path = os.path.join('data', 'unprocessed', 'AAPL' + date + '.csv')
-    records = csv_to_matrices.load_csv_file(csv_data_path)
+    records = compressor.load_csv_file(csv_data_path)
 
-    full_d_asks = csv_to_matrices.process_side_records('ASK', floor_price_round, side_num_layers, side_num_price_levels,
-                                                       side_minutes_per_day, records)[:, :, 330:720]  # working hours
-    full_d_bids = csv_to_matrices.process_side_records('BID', floor_price_round, side_num_layers, side_num_price_levels,
-                                                       side_minutes_per_day, records)[:, :, 330:720]
+    full_d_asks = compressor.process_side_records('ASK', floor_price_round, side_num_layers, side_num_price_levels,
+                                                  side_minutes_per_day, records)[:, :, 330:720]  # working hours
+    full_d_bids = compressor.process_side_records('BID', floor_price_round, side_num_layers, side_num_price_levels,
+                                                  side_minutes_per_day, records)[:, :, 330:720]
 
-    d = csv_to_matrices.get_d(d_num_layers, d_num_price_levels, d_minutes_per_day, full_d_asks, full_d_bids)
+    d = compressor.get_d(d_num_layers, d_num_price_levels, d_minutes_per_day, full_d_asks, full_d_bids,
+                         side_start_trading_session_index)
 
     npy_data_path = os.path.join('data', 'processed', 'AAPL' + date + '.npy')
     np.save(npy_data_path, d)